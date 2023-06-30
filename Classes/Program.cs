ba763273924a54925c954e0857ddc9a8f025e8f7
Bryan Levin <bryanlevin72@gmail.com>
2023-06-30 08:27:09 -0700

    Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Make = "Honda";
+            car1.Model = "Civic";
+            car1.Year = 2019;
+
+            Console.WriteLine($"Make:" + car1.Make);
+            Console.WriteLine($"Model:" + car1.Model);
+            Console.WriteLine($"Year:" + car1.Year);
         }
     }
 }
